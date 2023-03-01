9539a9d47dfd4eaf318a091ab8926154e1ce69a4
Alejo Santis <alejosantismk11@gmail.com>
2023-03-01 11:23:38 -0500

    clase03 completada

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const btnAdd = document.querySelector("[data-form-btn]");
 const inputTask = document.querySelector("[data-form-input]");
+const taskList = document.querySelector("[data-list]");
 
 const createTask = (e) => {
   e.preventDefault();
-  console.log(inputTask.value);
+  const value = inputTask.value;
+  const taskli = document.createElement("li");
+  taskli.classList.add("card");
+  inputTask.value = "";
+  const content = `
+    <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  taskli.innerHTML = content;
+  taskList.appendChild(taskli);
 };
 
 btnAdd.addEventListener("click", createTask);
